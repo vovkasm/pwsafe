44a588194dfcd55fd8f80fc2c3660420c634933a
Vladimir Timofeev <vovkasm@gmail.com>
2020-12-24 01:06:07 +0300

    cmake: register localization as resources

--- CMakeModules/PwsafeResources.cmake
+++ CMakeModules/PwsafeResources.cmake
@@ -5,12 +5,13 @@ include(CmakeParseArguments)
 
 function(pwsafe_register_resource resource_target)
   set(options)
-  set(singleArgs NAME LOCATION)
+  set(singleArgs NAME LOCATION INSTALL_DIR)
   set(multiArgs)
   cmake_parse_arguments(res "${options}" "${singleArgs}" "${multiArgs}" ${ARGN})
 
   set_property(TARGET ${resource_target} APPEND PROPERTY RESOURCE_NAMES "${res_NAME}")
   set_property(TARGET ${resource_target} PROPERTY "RESOURCE_LOCATION_${res_NAME}" "${res_LOCATION}")
+  set_property(TARGET ${resource_target} PROPERTY "RESOURCE_INSTALL_DIR_${res_NAME}" "${res_INSTALL_DIR}")
 endfunction()
 
 function(pwsafe_add_resources target)
@@ -28,9 +29,15 @@ function(pwsafe_add_resources target)
   get_property(res_names TARGET ${resource_target} PROPERTY RESOURCE_NAMES)
   foreach(name ${res_names})
     get_property(file TARGET ${resource_target} PROPERTY "RESOURCE_LOCATION_${name}")
+    get_property(install_dir TARGET ${resource_target} PROPERTY "RESOURCE_INSTALL_DIR_${name}")
+    if (DEFINED install_dir)
+      set(macosx_package_location "Resources/${install_dir}")
+    else ()
+      set(macosx_package_location "Resources")
+    endif()
     set_source_files_properties(${file} PROPERTIES
       GENERATED 1
-      MACOSX_PACKAGE_LOCATION Resources
+      MACOSX_PACKAGE_LOCATION ${macosx_package_location}
       )
     target_sources(${target} PUBLIC ${file})
   endforeach()
